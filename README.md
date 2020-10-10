9d8d213453cf18d3251420c7d5a01fd27ae72774
Jacob Emil Ulvedal Rosborg <jacob@rosborg.dk>
2020-10-10 14:55:53 +0200

    changes

---
+++ README.md
@@ -0,0 +1,11 @@
+This project usaes postgres and the postgres extensions CITEXT and SEMVER.
+
+[CITEXT](https://pgxn.org/dist/citext/2.0.2/doc/citext.html) \
+[SEMVER](https://pgxn.org/dist/semver/doc/semver.html)
+
+# TODO
+[x] publish and upload
+[ ] authentication on publish
+[ ] authentication tokens
+[ ] download
+[ ] git http repo (github worker)
--- migrations/V1__crate.sql
+++ migrations/V1__crate.sql
@@ -1,13 +1,18 @@
-CREATE TABLE crate(
-    name CITEXT NOT NULL,
-    PRIMARY KEY (name),
-    CONSTRAINT valid_name CHECK ( name ~= '[A-Za-z_-][A-Za-z0-9_-]*' )
+CREATE TABLE "crate"(
+    "name" CITEXT NOT NULL,
+    "categories" CITEXT[] NOT NULL,
+    "keywords" CITEXT[] NOT NULL,
+    PRIMARY KEY ("name"),
+    CONSTRAINT "valid_name" CHECK ( "name" ~= '[A-Za-z_-][A-Za-z0-9_-]*' )
 );
 
-CREATE TABLE crate_version(
-    crate CITEXT NOT NULL,
-    download VARCHAR NOT NULL,
+CREATE INDEX "crate_categories_idx" on "crate" USING GIN ("categories");
+CREATE INDEX "crate_keywords_idx" on "crate" USING GIN ("keywords");
+
+CREATE TABLE "crate_version"(
+    "crate" CITEXT NOT NULL,
+    "download" VARCHAR NOT NULL,
     "version" SEMVER NOT NULL,
-    PRIMARY KEY (crate, "version"),
-    FOREIGN KEY (crate) REFERENCES crate (name)
+    PRIMARY KEY ("crate", "version"),
+    FOREIGN KEY ("crate") REFERENCES "crate" ("name")
 );
